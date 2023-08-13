243a8f1ddd0b2cb8fcb22d5351b1585cf1298de6
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-08-13 11:06:02 -0400

    update image

--- python/qr.py
+++ python/qr.py
@@ -8,7 +8,7 @@ from PIL import Image
 # Fungsi untuk menambahkan gambar ditengah QR Code
 def addLogo(qr_code, gambar):
     qr_code = qr_code.convert("RGBA")
-    gambar = gambar.resize((80, 80))  # Ubah ukuran gambar sesuai kebutuhan
+    gambar = gambar.resize((120, 120))  # Ubah ukuran gambar sesuai kebutuhan
     posisi = ((qr_code.size[0] - gambar.size[0]) // 2, (qr_code.size[1] - gambar.size[1]) // 2)
     qr_code.paste(gambar, posisi, gambar)
     return qr_code
@@ -19,7 +19,7 @@ whatsapp_link = "085274620281"
 # Tautan YouTube
 github_link = "github.com/bobbyrahmanda13"
 # Tautan Instagram
-instagram_link = "instagram.com/rahman.rvbr"
+instagram_link = "instagram.com/0.0_rahman_0.0/"
 # Tautan Telegram
 telegram_link = "t.me/Rahman_0000"
 # Tautan Website
@@ -41,9 +41,9 @@ qr_image = qr.make_image(
     image_factory=StyledPilImage, 
     module_drawer=CircleModuleDrawer(), eye_drawer=CircleModuleDrawer(), 
     # color_mask=SolidFillColorMask(back_color=(255,255,255), front_color=(0,0,200))
-    color_mask=RadialGradiantColorMask(back_color=(255,255,255), edge_color=(0,200,0), center_color=(0,0,0))
+    color_mask=RadialGradiantColorMask(back_color=(255,255,255), edge_color=(0,0,0), center_color=(100,100,100))
 )
 
-logo = Image.open("g.png")
+logo = Image.open("image/gear5.png")
 qrCodeWithImage = addLogo(qr_image, logo)
 qrCodeWithImage.save("qr_code.png")
